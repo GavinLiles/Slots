4b45a0ad6ac4b883b4f423f1ce183da10914a14a
Gavin Liles <gavinliles@gmail.com>
2025-01-15 21:56:05 -0600

    random

--- test.cpp
+++ test.cpp
@@ -1,8 +1,25 @@
 #include<iostream>
 #include <array>
-int maxH = 4;
-int maxW = 4;
+#include <random>
+#include <ctime>
+int const maxH = 4;
+int const maxW = 4;
+std::wstring const  unicodeArray[10] = {
+        L"\U0001F349", // Watermelon
+        L"\U0001F34C", // Banana
+        L"\U0001F34D", // Pineapple
+        L"\U0001F34F", // Green Apple
+        L"\U0001F32D", // Hot Dog
+        L"\U0001F32E", // Taco
+        L"\U0001F33D", // Ear of Corn
+        L"\U0001F344", // Mushroom
+        L"\U0001F345", // Tomato
+        L"\U0001F346"  // Eggplant
+    };
 int main(){
+    //random seed
+    std::srand(time(NULL));
+
 // 4*4 GAME
 //symbols 10
 //symbol 1 = U+1F349
@@ -19,10 +36,15 @@ int main(){
 
 //code
 //an array to do the slots
-std::array<std::array<int,4>,4> gav;
+std::array<std::array<std::wstring,maxH>,maxW> gav;
 for(int i=0; i < maxH; i++ ){
     for(int j=0; j < maxH; j++ ){
-        gav.at(i).at(j) = 1;
+        gav.at(i).at(j) = unicodeArray[rand() %10];
+    }
+}
+for(int i=0; i < maxH; i++ ){
+    for(int j=0; j < maxH; j++ ){
+       std::wcout << gav.at(i).at(j);
     }
 }
 }
