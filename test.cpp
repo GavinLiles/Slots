99868e44c8d1346303f33f509642105029ce9953
Clayton Bruce <claytonleobruce@gmail.com>
2025-01-15 21:48:09 -0600

    added symbols

--- test.cpp
+++ test.cpp
@@ -5,7 +5,16 @@ int maxW = 4;
 int main(){
 // 4*4 GAME
 //symbols 10
-//symbol 1 = U+1F4A3
+//symbol 1 = U+1F349
+//symbol 2 = U+1F34C
+//symbol 3 = U+1F34D
+//symbol 4 = U+1F34F
+//symbol 5 = U+1F32D
+//symbol 6 = U+1F32E
+//symbol 7 = U+1F33D
+//symbol 8 = U+1F344
+//symbol 9 = U+1F345
+//symbol 10 = U+1F346
 
 
 //code
